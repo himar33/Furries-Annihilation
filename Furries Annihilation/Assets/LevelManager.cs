91c9d82b2140accba80e2b060fc50c945788f4f2
himarsus <60131783+himar33@users.noreply.github.com>
2022-05-12 12:25:29 +0200

    Day and Night cycle added

--- Furries Annihilation/Assets/LevelManager.cs
+++ Furries Annihilation/Assets/LevelManager.cs
@@ -1,18 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    enum GameState
+    {
+        DAY,
+        NIGHT,
+        TRANSTION,
+    }
+    
+    [SerializeField]
+    private GameState state;
+    [SerializeField]
+    private float time;
+    [SerializeField]
+    private float timeSpeed;
+
     void Start()
     {
-        
+        state = GameState.DAY;
+        time = 0.0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case GameState.DAY:
+                UpdateDay();
+                break;
+            case GameState.NIGHT:
+                UpdateNight();
+                break;
+            default:
+                break;
+        }
+
+        UpdateLighting(time);
+
+        CheckTimeValue();
+    }
+
+    private void UpdateNight()
+    {
+        time -= timeSpeed * Time.deltaTime;
+    }
+
+    private void UpdateDay()
+    {
+        time += timeSpeed * Time.deltaTime;
+    }
+
+    private void CheckTimeValue()
+    {
+        if (time >= 1)
+        {
+            state = GameState.NIGHT;
+        }
+        else if (time <= 0)
+        {
+            state = GameState.DAY;
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.skybox.SetFloat("_CubemapTransition", timePercent);
     }
 }
