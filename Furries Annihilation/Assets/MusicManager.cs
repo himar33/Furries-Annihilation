5f2582cbc9ab9af296acaee5933de1f421d2c939
himar33 <himbrgo@iesvalldemossa.net>
2022-05-13 11:45:14 +0200

    COSITAS JAJAJAJJAJAJAJ

---
+++ Furries Annihilation/Assets/MusicManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioClip> audioClips;
+}
