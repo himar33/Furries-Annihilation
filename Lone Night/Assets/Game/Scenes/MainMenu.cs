96f6586afed5e1296124fc9e9f6756c68073d0ee
Pol Vazquez Porcar <60131834+AMADE128@users.noreply.github.com>
2022-06-07 20:26:09 +0200

    Added Menu Scene

---
+++ Lone Night/Assets/Game/Scenes/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Play!");
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
